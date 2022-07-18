950b1a8c4fac06e1db32f8a33eec675659cbcddf
Humaiun Kabir <humaiunkabir@outlook.com>
2022-07-18 10:33:22 +0600

    For media correction completed for deploy.

--- djangoblog/djangoblog/urls.py
+++ djangoblog/djangoblog/urls.py
@@ -15,14 +15,17 @@ Including another URLconf
 """
 from django import views
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('blogapp.urls')),
     path('admin/', admin.site.urls),
-]  
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT}),
+]
 
-urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
